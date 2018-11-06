0a1fb3a6399d330c7cfe8dde55dc73b49de66d27
johndanielsevilla <jsev0592@uni.sydney.edu.au>
2018-11-07 00:53:13 +1100

    renamed variables and removed useless code

--- sockets.go
+++ sockets.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-)
 
+	"github.com/gorilla/websocket"
+)
 
 // MessageHandler : relays messages to users in a hub
 func (h *Hub) MessageHandler() {
-
 	for {
 		msg := <-h.broadcast
-
 		for client := range h.clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
@@ -21,24 +20,22 @@ func (h *Hub) MessageHandler() {
 				delete(h.clients, client)
 			}
 		}
-
 	}
-
 }
 
+// TODO: resolve data races
 
 // ConnectionHandler : connects the socket to the requested hub
 func ConnectionHandler(w http.ResponseWriter, r *http.Request) {
 
-	// var tokenParam []string
-	var hidParam []string
+	var tokenParam, hidParam []string
 	var ok bool
 
-	// tokenParam, ok = r.URL.Query()["token"]
-	// if !ok || len(tokenParam) < 1 {
-	// 	http.Error(w, "400 - no token specified!", http.StatusBadRequest)
-	// 	return
-	// }
+	tokenParam, ok = r.URL.Query()["token"]
+	if !ok || len(tokenParam) < 1 {
+		http.Error(w, "400 - token invalid!", http.StatusBadRequest)
+		return
+	}
 
 	hidParam, ok = r.URL.Query()["hub"]
 	if !ok || len(hidParam) < 1 {
@@ -46,47 +43,53 @@ func ConnectionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// tok := tokenParam[0]
+	tok := tokenParam[0]
 	hid := hidParam[0]
 
-	// ok = validateToken(tok)
-	// if !ok {
-	// 	http.Error(w, "403 - token invalid!", http.StatusForbidden)
-	// 	return
-	// }
+	ok = validateToken(tok)
+	if !ok {
+		http.Error(w, "400 - token invalid!", http.StatusBadRequest)
+		return
+	}
 
-	// // validate hub
-	// hub := getHub(hid)
-	// if hub == nil {
-	// 	// http.Error(w, "400 - hub doesn't exist!", http.StatusForbidden)
-	// 	hub = createHub(hid)
-	// 	go hub.MessageHandler()
-	// 	return
-	// }
+	u := getUserFromToken(tok)
+	if u == nil {
+		http.Error(w, "500 - error fetching user!", http.StatusInternalServerError)
+		return
+	}
 
+	// join an existing hub otherwise create it
+	h := getHub(hid)
+	if h == nil {
+		h = u.createHub(hid)
+		go h.MessageHandler()
+		fmt.Println("created new hub")
+	} else {
+		h.joinUser(u)
+		fmt.Println("hub exists, adding new user to it")
+	}
 
-	hub := createHub(hid)
+	ws, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
 
-	fmt.Println("made it here")
-
-	// upgrade http connection to websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ws.Close()
-
-	hub.clients[ws] = true
+	h.clients[ws] = true
 
 	for {
 		var msg Message
 		err := ws.ReadJSON(&msg)
+		fmt.Println(msg)
+
+		h.recordMessage(&msg)
+
 		if err != nil {
 			log.Printf("error: %v", err)
-			delete(hub.clients, ws)
+			delete(h.clients, ws)
 			break
 		}
-		hub.broadcast <- msg
+		h.broadcast <- msg
 	}
 
 }
